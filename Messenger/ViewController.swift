3353cc0ab5b11c78b6cecb5ebb3b6a7c834de150
JeampierCM <jeampier.cornejo@tecsup.edu.pe>
2022-12-10 07:25:43 -0800

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by jeampier on 12/10/22.
+//  Copyright © 2022 miempresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
